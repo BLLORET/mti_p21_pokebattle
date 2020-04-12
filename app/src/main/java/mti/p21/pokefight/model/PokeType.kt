4ed73f9bf39c5bbaba9d9bb6bf2e2e62e8c6cdf1
Baptiste Lloret <baptiste.loret@hotmail.fr>
2020-04-12 21:58:43 +0200

    [Help] Add help fragment

--- app/src/main/java/mti/p21/pokefight/model/PokeType.kt
+++ app/src/main/java/mti/p21/pokefight/model/PokeType.kt
@@ -5,11 +5,12 @@ import android.content.res.Resources
 import android.graphics.drawable.Drawable
 import android.util.Log
 import mti.p21.pokefight.R
+import java.io.Serializable
 
 /**
  * Class that represent the pokemon type.
  */
-data class PokeType (val name : String) {
+data class PokeType (val name : String) : Serializable {
 
     /**
      * Return the id of the picture associated with the specific PokeType.
