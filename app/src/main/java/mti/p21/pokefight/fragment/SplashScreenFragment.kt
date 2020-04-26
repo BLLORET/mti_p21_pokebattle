3a821890286673fcaf15dc58669a5d0949ea23a2
Nicolas Acart <nicolas.acart@epita.fr>
2020-04-26 02:45:56 +0200

    [REFACTO] use datalive instead of using callbacks etc

--- app/src/main/java/mti/p21/pokefight/fragment/SplashScreenFragment.kt
+++ app/src/main/java/mti/p21/pokefight/fragment/SplashScreenFragment.kt
@@ -1,42 +1,36 @@
 package mti.p21.pokefight.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_splash_screen.*
-import mti.p21.pokefight.MainActivity
 import mti.p21.pokefight.R
+import mti.p21.pokefight.utils.AbstractActivity
+import mti.p21.pokefight.utils.AbstractFragment
 
 /**
  * [SplashScreenFragment] represent the fragment of the splash screen view.
  */
-class SplashScreenFragment : Fragment() {
+class SplashScreenFragment(a: AbstractActivity) : AbstractFragment(a) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
-    }
+    override val layoutResource = R.layout.fragment_splash_screen
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Disabled them if pokemons are not loaded
-        if ((activity as MainActivity).data == null) {
-            btn_battle.isEnabled = false
-            btn_pokedex.isEnabled = false
+        btn_battle.isEnabled = false
+        btn_pokedex.isEnabled = false
+        mainActivity.executeWhenListPokemonIsLoaded(this) {
+            btn_battle.isEnabled = true
+            btn_pokedex.isEnabled = true
         }
 
         btn_battle.setOnClickListener {
-            (activity as MainActivity).onBattleButtonClicked()
+            mainActivity.onBattleButtonClicked()
         }
 
         btn_pokedex.setOnClickListener {
-            (activity as MainActivity).onPokedexButtonClicked()
+            mainActivity.onPokedexButtonClicked()
         }
     }
 }
